24789c1e6cebbf5cc659e6bdf1a1b3f833c67c71
SAdevelopersSpace <110198844+SAdevelopersSpace@users.noreply.github.com>
2022-08-03 16:31:29 +0200

    Create menu.js

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+const links = {
+  block1: [
+      { label: "Content 1 ", url: "Introduction_Pages/main_portfolio.html"},
+    
+  ]
+}
+
+const ol = document.getElementById('block');   
+links.forEach(link => {
+  let li = document.createElement('li');
+  let a = document.createElement('a');
+  a.setAttribute('href', link.url);
+  a.innerText = link.label;
+  li.appendChild(a);
+  ol.appendChild(li);
+});
